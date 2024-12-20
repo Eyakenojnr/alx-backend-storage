ac8f346a53a9bb5144a0cd9a40c985dd2885709b
Uduak Umanah <eyakenojnr@gmail.com>
2024-12-20 06:43:49 +0100

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-This module have a utility function that list all document
+This module have a utility function that lists all documents in a collection.
 """
 import pymongo
 
 
 def list_all(mongo_collection):
     """
-    list all collections
+    list all documents in a collection
     """
-    if not mongo_collection:
-        return []
-    return list(mongo_collection.find())
+    if mongo_collection is not None:
+        return list(mongo_collection.find())
+    return []
 
